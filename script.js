b2d3f4dddd45d9932fb70c5a45cf26a8528ec47b
Kittipong Pounglaya <kp.Pounglaya@gmail.com>
2023-08-25 20:20:02 +0700

    commit

--- script.js
+++ script.js
@@ -2,25 +2,37 @@ document.addEventListener("DOMContentLoaded", function () {
   const scrollContainers = document.querySelectorAll(".scroll-container");
 
   scrollContainers.forEach((scrollContainer) => {
-    let isMouseDown = false;
+    let isInteracting = false;
     let startX;
     let scrollLeft;
 
-    scrollContainer.addEventListener("mousedown", (e) => {
-      isMouseDown = true;
-      startX = e.clientX;
+    const startInteraction = (event) => {
+      isInteracting = true;
+      startX =
+        event.type === "touchstart" ? event.touches[0].clientX : event.clientX;
       scrollLeft = scrollContainer.scrollLeft;
-    });
+    };
 
-    scrollContainer.addEventListener("mousemove", (e) => {
-      if (!isMouseDown) return;
-      const x = (e.clientX - startX) * 3; // Adjust the multiplier for smoother scrolling
+    const moveInteraction = (event) => {
+      if (!isInteracting) return;
+      const clientX =
+        event.type === "touchmove" ? event.touches[0].clientX : event.clientX;
+      const x = (clientX - startX) * 3; // Adjust the multiplier for smoother scrolling
       scrollContainer.scrollLeft = scrollLeft - x;
-    });
+    };
 
-    document.addEventListener("mouseup", () => {
-      isMouseDown = false;
-    });
+    const endInteraction = () => {
+      isInteracting = false;
+    };
+
+    scrollContainer.addEventListener("mousedown", startInteraction);
+    scrollContainer.addEventListener("touchstart", startInteraction);
+
+    scrollContainer.addEventListener("mousemove", moveInteraction);
+    scrollContainer.addEventListener("touchmove", moveInteraction);
+
+    document.addEventListener("mouseup", endInteraction);
+    document.addEventListener("touchend", endInteraction);
 
     scrollContainer.addEventListener("scroll", () => {
       const maxScroll =
@@ -33,3 +45,47 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    // console.log(e); #pointer for each event
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    // console.log(href); #get(id) #,#how
+
+    // scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      // console.log(sectionEl); #get in click section on top
+      sectionEl.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  });
+});
+
+const sectionHeroEl = document.querySelector(".hero-section");
+console.log(sectionHeroEl);
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) document.body.classList.add("sticky");
+    if (ent.isIntersecting === true) document.body.classList.remove("sticky");
+  },
+  {
+    //In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
